c4bc71ab001d92f2265d7a24d72fe65286fcf28b
Sushant Mahajan <smahajan@cse.iitb.ac.in>
2016-08-15 19:54:40 +0530

    cleanup

--- bash/termStart.sh
+++ bash/termStart.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 zenity --title Killer --text "Kill existing drop downs?" --question >& /dev/null
 if [[ $? -eq 0 ]]; then
-	a=( `pgrep -f dropdown` );
-	terminator -b --layout=dropdown --profile=solar &
-	kill -9 $a
+	a=( `pgrep -f dropdown` )
+	terminator -b --layout=dropdown --profile=solar & 
+	! [[ -z $a ]] && kill -9 $a
 else 
 	terminator -b --layout=dropdown --profile=solar &
 fi

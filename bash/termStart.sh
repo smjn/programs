3133fd06a0258525c910cf5d51037b3de30ca5b8
Sushant Mahajan <smahajan@cse.iitb.ac.in>
2016-08-15 19:49:58 +0530

    kill multiple terminators if present

--- bash/termStart.sh
+++ bash/termStart.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 zenity --title Killer --text "Kill existing drop downs?" --question >& /dev/null
 if [[ $? -eq 0 ]]; then
-	a=$(pgrep -f dropdown);
+	a=( `pgrep -f dropdown` );
 	terminator -b --layout=dropdown --profile=solar &
 	kill -9 $a
 else 

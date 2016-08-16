ece66cad792e3f324d216c86a944f17739e2a3f3
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-08-16 23:25:45 +0530

    removed conditional

--- bash/termStart.sh
+++ bash/termStart.sh
@@ -4,7 +4,7 @@ if [[ $? -eq 0 ]]; then
 	zenity --title Killer --text "Kill existing drop downs?" --question >& /dev/null
 	if [[ $? -eq 0 ]]; then
 		terminator -b --layout=dropdown --profile=solar & 
-		! [[ -z $a ]] && kill -9 $a
+		kill -9 $a
 	else 
 		terminator -b --layout=dropdown --profile=solar &
 	fi

851e6a5cfab99ac808cdd27b2be6eeb2df4e3829
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-08-17 11:34:51 +0530

    reverted, changes failed

--- bash/termStart.sh
+++ bash/termStart.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 a=( `pgrep -f dropdown` )
-err=$?
-terminator -H -b --layout=dropdown --profile=solar 
-if [[ ${err} -eq 0 ]]; then
+if [[ $? -eq 0 ]]; then
 	zenity --title Killer --text "Kill existing drop downs?" --question >& /dev/null
-	[[ $? -eq 0 ]] && kill -9 $a
+	if [[ $? -eq 0 ]]; then
+		terminator -b --layout=dropdown --profile=solar & 
+		! [[ -z $a ]] && kill -9 $a
+	else 
+		terminator -b --layout=dropdown --profile=solar &
+	fi
+else
+	terminator -b --layout=dropdown --profile=solar &
 fi

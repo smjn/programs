a84fd7c40d1df4713d7038d54f27929c5f6919e5
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-08-17 21:27:15 +0530

    checked tab2space

--- bash/fixvertex.sh
+++ bash/fixvertex.sh
@@ -3,15 +3,15 @@ loc=(/usr/share/themes/Vertex /usr/share/themes/Vertex-Dark /usr/share/themes/Ve
 file=(gtk.css gtk-dark.css)
 for i in ${loc[@]}
 do
-	for f in ${file[@]}
-	do
-		name=$i/gtk-3.0/$f
-		if ! [[ -e "$name" ]]; then
-			echo $name does not exit
-			continue
-		fi
-		sudo sed -i 's/\(-gtk.*pan-.*;\)\s\+\}$/\/\*\1\*\/ \}/g' $name
-		sudo sed -i 's/\(-gtk.*pan-.*;$\)/\/\*\1\*\//g' $name
-	done
+    for f in ${file[@]}
+    do
+        name=$i/gtk-3.0/$f
+        if ! [[ -e "$name" ]]; then
+            echo $name does not exit
+            continue
+        fi
+        sudo sed -i 's/\(-gtk.*pan-.*;\)\s\+\}$/\/\*\1\*\/ \}/g' $name
+        sudo sed -i 's/\(-gtk.*pan-.*;$\)/\/\*\1\*\//g' $name
+    done
 done
 

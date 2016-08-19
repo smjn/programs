dc6d397cb12308996714dc5752d4fb7fa39dcf8c
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-08-19 19:12:10 +0530

    added code to grep days left in billing cycle

--- bash/airtel.sh
+++ bash/airtel.sh
@@ -1,1 +1,2 @@
-wget -O - 'http://122.160.230.125:8080/planupdate/' 2>/dev/null | grep -A2 'You are left <'|grep -Pio '\d+[.]\d+\s+GB'
+#!/bin/bash
+wget -O - 'http://122.160.230.125:8080/planupdate/' 2>/dev/null | grep -P -A2 'You are left <|No. of days left'|grep -Pio '\d+[.]\d+\s+GB|\d+ day\(s\)'

71b0625b75d6e93a80d63433d4fa8bf590f62184
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-08-20 17:51:32 +0530

    start terminator hidden

--- bash/termStart.sh
+++ bash/termStart.sh
@@ -3,11 +3,11 @@ a=( `pgrep -f dropdown` )
 if [[ $? -eq 0 ]]; then
 	zenity --title Killer --text "Kill existing drop downs?" --question >& /dev/null
 	if [[ $? -eq 0 ]]; then
-		terminator -b --layout=dropdown --profile=solar & 
+		terminator -bH --layout=dropdown --profile=solar & 
 		! [[ -z $a ]] && kill -9 $a
 	else 
-		terminator -b --layout=dropdown --profile=solar &
+		terminator -bH --layout=dropdown --profile=solar &
 	fi
 else
-	terminator -b --layout=dropdown --profile=solar &
+	terminator -bH --layout=dropdown --profile=solar &
 fi

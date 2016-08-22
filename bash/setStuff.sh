c37543233d726509c7ad9375d267370fb932640f
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-08-22 20:17:33 +0530

    added a startup script

---
+++ bash/setStuff.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#synclient TouchpadOff=1
+#mid=$(xinput |grep -iE 'microsoft.*slave[ ]+pointer'|grep -oE 'id=[0-9]+'|tail -1|cut -d= -f2)
+#mid2=$(xinput |grep -iE 'microsoft.*slave[ ]+pointer'|grep -oE 'id=[0-9]+'|head -1|cut -d= -f2)
+#xinput --set-prop $mid "Device Accel Constant Deceleration" 2.7
+#xinput --set-prop $mid2 "Device Accel Constant Deceleration" 2.7
+xmodmap /home/sushant/.Xmodmap
+notify-send "Stuff is set"
+
+

86fecc53ffc4a043a2cc7e9a8d535b688ffb4762
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-12-06 18:45:29 +0530

    added lock script

---
+++ bash/locker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#turn off screen after n seconds and then run i3lock
+[[ $# -ne 2 ]] && { echo "usage: ./locker.sh <seconds> <path/to/png/image>"; exit 1; }
+xset dpms $1
+i3lock -f -i $2 
--- bash/sense.sh
+++ bash/sense.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -n "Core: "
+echo -n "CPU:"
 sensors 2>/dev/null|grep -iEo 'physical id 0:\s+\+[0-9]{2}'|grep -iEo '\+[0-9]+'|tr '\n' '.'
-echo -n "0°C Fan: "
+echo -n "0°C, "
 sensors 2>/dev/null|grep -io 'fan.*RPM'|grep -Eo '[0-9]+ RPM'|tr '\n' ' '

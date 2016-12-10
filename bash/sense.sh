37ff82698493c7eb02df2abb683a6851a2ee5761
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-12-11 00:44:28 +0530

    changed script to show GPU temp

--- bash/sense.sh
+++ bash/sense.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo -n "CPU:"
 sensors 2>/dev/null|grep -iEo 'physical id 0:\s+\+[0-9]{2}'|grep -iEo '\+[0-9]+'|tr '\n' '.'
-echo -n "0°C, "
+echo -n "0°C,GPU:"
+sensors 2>/dev/null|grep -iEo 'temp1:\s+\+[0-9]{2}'|grep -iEo '\+[0-9]+'|tr '\n' '.'
+echo -n "0°C,"
 sensors 2>/dev/null|grep -io 'fan.*RPM'|grep -Eo '[0-9]+ RPM'|tr '\n' ' '

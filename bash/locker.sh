7c06357f1d99b2a63aeff745d438521bb23084ad
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-12-12 00:56:49 +0530

    awake on unlock

--- bash/locker.sh
+++ bash/locker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #turn off screen after n seconds and then run i3lock
 [[ $# -ne 2 ]] && { echo "usage: ./locker.sh <seconds> <path/to/png/image>"; exit 1; }
-xset dpms $1
-i3lock -f -i $2 
-xset dpms 0 0 0
+revert() {
+  xset dpms 0 0 0
+}
+trap revert SIGHUP SIGINT SIGTERM
+xset +dpms dpms 5 5 5
+i3lock -n -f -i $2
+revert

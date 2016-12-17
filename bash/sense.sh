b6d8e81ca7c5f4225cfc4db381e5647377b8eadc
Sushant Mahajan <sushant.mahajan88@gmail.com>
2016-12-17 13:17:43 +0530

    added spacing in sense.sh

--- bash/sense.sh
+++ bash/sense.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo -n "CPU:"
 sensors 2>/dev/null|grep -iEo 'physical id 0:\s+\+[0-9]{2}'|grep -iEo '\+[0-9]+'|tr '\n' '.'
-echo -n "0°C,GPU:"
+echo -n "0°C, GPU:"
 sensors 2>/dev/null|grep -iEo 'temp1:\s+\+[0-9]{2}'|grep -iEo '\+[0-9]+'|tr '\n' '.'
-echo -n "0°C,"
+echo -n "0°C, "
 sensors 2>/dev/null|grep -io 'fan.*RPM'|grep -Eo '[0-9]+ RPM'|tr '\n' ' '

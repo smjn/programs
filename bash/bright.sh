42bccc1b243061dd57650b99b18bd6762661d743
Sushant Mahajan <sushant.mahajan88@gmail.com>
2017-01-07 21:33:19 +0530

    shortened brightness code

--- bash/bright.sh
+++ bash/bright.sh
@@ -4,16 +4,11 @@
 #ALL	ALL = (ALL)	NOPASSWD: /usr/bin/tee /sys/class/backlight/intel_backlight/brightness
 #in /etc/sudoers
 
-old=$(cat /sys/class/backlight/intel_backlight/brightness)
-mx=$(cat /sys/class/backlight/intel_backlight/max_brightness)
-tobe=0
-if [[ $# -eq 0 ]]; then
-	tobe=$(($old+400))
-else
-	tobe=$(($old-400))
-fi
-
-new=$(($tobe>$mx?$mx:($tobe<0?400:$tobe)))
+pref="/sys/class/backlight/intel_backlight"
+old=$(cat $pref/brightness)
+mx=$(cat $pref/max_brightness)
+new=$(($#==0?$old + 400:$old - 400))
+new=$(($new>$mx?$mx:($new<0?400:$new)))
 echo $new|sudo /usr/bin/tee /sys/class/backlight/intel_backlight/brightness
 
 

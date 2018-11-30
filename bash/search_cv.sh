0d2ce9be5fbf9647018785bb3ab92a97422f1e6c
Sushant Mahajan <sushant.mahajan@sap.com>
2018-11-30 11:55:58 +0530

    search res

---
+++ bash/search_cv.sh
@@ -0,0 +1,121 @@
+#!/usr/bin/env bash
+
+(( flag=0x0000 ))
+(( initf=0x1110 ))
+(( keyf=0x0011 ))
+src=''
+dst=''
+keyw=''
+checkPreReq() {
+    which pdftotext >/dev/null 2>&1|| { echo "for mac use: brew install psdtotext, for linux apt install poppler-utils; pdftotext not present"; exit 1; }
+    which grep >/dev/null 2>&1 || { echo "grep not present"; exit 1; }
+    which find >/dev/null 2>&1 || { echo "find not present"; exit 1; }
+}
+
+usage() {
+    echo "$0 [-i -s -d]|[-k -d]"
+    echo "to initialize"
+    echo './search_cv.sh -i -s <src resume dir> -d <dest res dir>'
+    echo "to search"
+    echo './search_cv.sh -d <dest res dir> -k <keyword1,keyword2,...>'
+    exit 2
+}
+
+initialize() {
+    if ! [[ -d "$src" ]]; then
+        echo "source does not exist/is not a directory"
+        exit 3
+    fi
+
+    if ! [[ -d "$dst" ]]; then
+        echo "destination does not exist/is not a directory"
+        exit 3
+    fi
+
+    find "$src" -iname "*.pdf" >/tmp/names3.txt
+    while read line; do
+        pdftotext $line $dst/$(basename $line).txt
+    done </tmp/names3.txt
+}
+
+finder() {
+    orig=$IFS
+    IFS=","
+    read -ra kws <<< "$keyw"
+    echo "${kws[@]}"
+    IFS=$orig
+    echo "individual"
+    for k in "${kws[@]}"; do
+        echo "$k:"
+        grep -irl "$k" "$dst"|sort|uniq
+    done
+
+    echo "or'ed keywords"
+    cmd="grep -irl"
+    for k in "${kws[@]}"; do
+        cmd=" $cmd -e $k"
+    done
+    cmd="$cmd $dst|sort|uniq"
+    eval $cmd
+
+    echo "anded keywords"
+    ind=0
+    for k in "${kws[@]}"; do
+        (( ind++ ))
+        if [[ $ind -eq 1 ]]; then 
+            grep -irl "$k" "$dst" >/tmp/names1.dat
+        fi
+        cat /tmp/names1.dat|xargs grep -il "$k" >/tmp/names2.dat
+        mv /tmp/names{2.dat,1.dat}
+    done
+    cat /tmp/names1.dat|sort|uniq
+}
+
+if [[ $# -eq 0 ]]; then
+    usage
+fi
+
+checkPreReq
+
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+    case $arg in
+        "-i")
+            (( flag=flag|0x1000 ))
+            shift
+            ;;
+        "-s")
+            (( flag=flag|0x0100 ))
+            shift
+            src="$1"
+            shift
+            ;;
+        "-d")
+            (( flag=flag|0x0010 ))
+            shift
+            dst="$1"
+            shift
+            ;;
+        "-k")
+            (( flag=flag|0x0001 ))
+            shift
+            keyw="$1"
+            shift
+            ;;
+        *)
+            echo "invalid arg: $arg"
+            exit 1
+            ;;
+    esac
+done
+
+if (( (flag&initf) == flag )); then
+    initialize
+elif (( (flag&keyf) == flag )); then
+    finder
+else
+    echo "invalid flag combination"
+    usage
+fi
+
+
